8e7b8348e66ea7ac5f160c9eabe921c26bcfb03f
Dcal-dev <kim9young@naver.com>
2022-08-24 14:59:51 +0900

    Add omega noise

--- TO_sim/Multi_m.py
+++ TO_sim/Multi_m.py
@@ -55,6 +55,7 @@ Rand_P = lambda N, noise: np.arange(1, N + 1) / (N + 1) + (1 / (N + 1)) * (noise
 Normal_P = lambda N, dinv: np.arange(1, N + 1) / (N + 1)
 Rand_P_NEW = lambda N, noise: normalization_del(Rand_P(N + 2, noise))
 Rand_P_Sort = lambda N, noise: np.sort(normalization(Rand_P(N + 2, noise)))[1:-1]
+Make_noise = lambda arr, D: arr + D * (2 * np.random.rand(len(arr)) - 1)
 
 
 def Make_Omega(p_method, N, Noise):
@@ -79,7 +80,7 @@ def Sim_Multi_mset(Noise, p_method="Normalize sorted perturbation"):
         Noise (float): Give noise at P_array to make perturbation at omega distribtion
         p_method (str, optional): "Sorted random", "Random", "Normalize sorted perturbation", "Normalize perturbation", "Perturbation" ,"Sorted perturbation". Defaults to "Normalize sorted perturbation".
     """
-    for m in [0.95,2,6]:
+    for m in [0.95, 2, 6]:
         N = 500
         K_start = 0.1
         K_end = 12.5
@@ -202,16 +203,14 @@ def Sim_Multi_mset(Noise, p_method="Normalize sorted perturbation"):
         )
 
 
-
-
-def Sim_Multi_m_giveNoise(m,Noise, p_method="Normalize sorted perturbation"):
+def Sim_Multi_m_giveNoise(m, Noise, p_method="Normalize sorted perturbation"):
     """_summary_
 
     Args:
         Noise (float): Give noise at P_array to make perturbation at omega distribtion
         p_method (str, optional): "Sorted random", "Random", "Normalize sorted perturbation", "Normalize perturbation", "Perturbation" ,"Sorted perturbation". Defaults to "Normalize sorted perturbation".
     """
-    
+
     N = 500
     K_start = 0.1
     K_end = 12.5
@@ -333,6 +332,7 @@ def Sim_Multi_m_giveNoise(m,Noise, p_method="Normalize sorted perturbation"):
         Folder_name=f"Review2/Control_P/{p_method}/Noise = {Noise}",
     )
 
+
 def Sim_Multi_mset_NOT_TQDM(Noise, p_method="Normalize sorted perturbation"):
     """_summary_
 
@@ -340,7 +340,7 @@ def Sim_Multi_mset_NOT_TQDM(Noise, p_method="Normalize sorted perturbation"):
         Noise (float): Give noise at P_array to make perturbation at omega distribtion
         p_method (str, optional): "Sorted random", "Random", "Normalize sorted perturbation", "Normalize perturbation", "Perturbation" ,"Sorted perturbation". Defaults to "Normalize sorted perturbation".
     """
-    for m in [0.95,2,6]:
+    for m in [0.95, 2, 6]:
         N = 500
         K_start = 0.1
         K_end = 12.5
@@ -464,7 +464,264 @@ def Sim_Multi_mset_NOT_TQDM(Noise, p_method="Normalize sorted perturbation"):
 
 
 if __name__ == "__main__":
-    Noise_list = list(map(int,input("Typing the Noise list")).split())
+    Noise_list = list(map(int, input("Typing the Noise list")).split())
     print(Noise_list)
     for Noise in Noise_list:
         Sim_Multi_mset(Noise)
+
+
+def Sim_Multi_mset_Noise(Noise):
+    """_summary_
+
+    Args:
+        Noise (float): Give noise at P_array to make perturbation at omega distribtion
+    """
+    for m in [0.95, 2, 6]:
+        N = 500
+        K_start = 0.1
+        K_end = 12.5
+        dK = 0.2
+        Ks = np.arange(K_start, K_end + dK, dK)
+        # m = 0.95
+        dtheta_init = np.zeros(N)
+        t_end = 1000
+        Noise_inv = 1 / Noise
+        dt = 0.1
+        dist = "Quantile Lorentzian"
+        if dist == "Normal":
+            theta_init, omega_init, Kc = Normal(N, 0, 1, seed=0)
+        elif dist == "Lorentzian":
+            theta_init, omega_init, Kc = Lorentzian(N, 0, 1, seed=0)
+        elif dist == "Quantile Lorentzian":
+            theta_init, omega_init, Kc = Quantile_Lorentzian(N, 0, 1, seed=0)
+
+        theta_dic = {}
+        dtheta_dic = {}
+        rs_dic = {}
+        omega_dic = {}
+        t_dic = {}
+
+        num = 0
+        for K in tqdm(Ks):
+            if num == 0:
+                omega = Make_noise(omega_init, Noise)
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta_init,
+                    p_dtheta=dtheta_init,
+                    p_omega=omega,
+                    distribution="Normal",
+                )
+            else:
+                omega = Make_noise(omega, Noise)
+                theta = Make_noise(theta_s[-1], Noise)
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta_dic[num - 1][-1],
+                    p_dtheta=dtheta_dic[num - 1][-1],
+                    p_omega=omega,
+                    distribution="Normal",
+                )
+                # theta_s,dtheta_s,omega,rs,t = Sol_Kuramoto_mf(N,K,m,(0,t_end),dt=dt,p_theta=theta_dic[num-1][-1],p_dtheta=dtheta_init,p_omega=omega_dic[num-1],distribution="Normal")
+            theta_dic[num] = theta_s
+            dtheta_dic[num] = dtheta_s
+            rs_dic[num] = rs
+            omega_dic[num] = omega
+            t_dic[num] = t + num * t_end
+            num += 1
+        theta_r_init, dtheta_r_init = theta_dic[num - 1][-1], dtheta_dic[num - 1][-1]
+
+        theta_r_dic = {}
+        dtheta_r_dic = {}
+        rs_r_dic = {}
+        omega_r_dic = {}
+        t_r_dic = {}
+        dKr = -0.2
+        Ksr = np.arange(K_end, K_start + dKr, dKr)
+
+        num_r = 0
+        for K in tqdm(Ksr):
+            if num_r == 0:
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta_r_init,
+                    p_dtheta=dtheta_r_init,
+                    p_omega=omega_init,
+                    distribution=dist,
+                )
+                # theta_s,dtheta_s,omega,rs,t = Sol_Kuramoto_mf(N,K,m,(0,t_end),dt=dt,p_theta=theta_r_init,p_dtheta=dtheta_init,p_omega=omega_init,distribution=dist)
+            else:
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta_r_dic[num_r - 1][-1],
+                    p_dtheta=dtheta_r_dic[num_r - 1][-1],
+                    p_omega=omega_r_dic[num_r - 1],
+                    distribution=dist,
+                )
+                # theta_s,dtheta_s,omega,rs,t = Sol_Kuramoto_mf(N,K,m,(0,t_end),dt=dt,p_theta=theta_r_dic[num_r-1][-1],p_dtheta=dtheta_init,p_omega=omega_r_dic[num_r-1],distribution=dist)
+            theta_r_dic[num_r] = theta_s
+            dtheta_r_dic[num_r] = dtheta_s
+            rs_r_dic[num_r] = rs
+            omega_r_dic[num_r] = omega
+            t_r_dic[num_r] = t + num_r * t_end
+            num_r += 1
+
+        createFolder(f"Review2/Noise/Noise = {Noise}")
+        Time_R(
+            Ks,
+            t_dic,
+            rs_dic,
+            t_r_dic,
+            rs_r_dic,
+            dK,
+            dt,
+            t_end,
+            N,
+            m,
+            mean_time=50,
+            save=True,
+            Folder_name=f"Review2/Noise/Noise = {Noise}",
+        )
+
+def Sim_Multi_mset_thetaNoise(Noise):
+    """_summary_
+
+    Args:
+        Noise (float): Give noise at P_array to make perturbation at omega distribtion
+    """
+    for m in [0.95, 2, 6]:
+        N = 500
+        K_start = 0.1
+        K_end = 12.5
+        dK = 0.2
+        Ks = np.arange(K_start, K_end + dK, dK)
+        # m = 0.95
+        dtheta_init = np.zeros(N)
+        t_end = 1000
+        Noise_inv = 1 / Noise
+        dt = 0.1
+        dist = "Quantile Lorentzian"
+        if dist == "Normal":
+            theta_init, omega_init, Kc = Normal(N, 0, 1, seed=0)
+        elif dist == "Lorentzian":
+            theta_init, omega_init, Kc = Lorentzian(N, 0, 1, seed=0)
+        elif dist == "Quantile Lorentzian":
+            theta_init, omega_init, Kc = Quantile_Lorentzian(N, 0, 1, seed=0)
+
+        theta_dic = {}
+        dtheta_dic = {}
+        rs_dic = {}
+        omega_dic = {}
+        t_dic = {}
+
+        num = 0
+        for K in tqdm(Ks):
+            if num == 0:
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta_init,
+                    p_dtheta=dtheta_init,
+                    p_omega=omega_init,
+                    distribution="Normal",
+                )
+            else:
+                theta = Make_noise(theta_s[-1], Noise)
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta,
+                    p_dtheta=dtheta_dic[num - 1][-1],
+                    p_omega=omega_init,
+                    distribution="Normal",
+                )
+                # theta_s,dtheta_s,omega,rs,t = Sol_Kuramoto_mf(N,K,m,(0,t_end),dt=dt,p_theta=theta_dic[num-1][-1],p_dtheta=dtheta_init,p_omega=omega_dic[num-1],distribution="Normal")
+            theta_dic[num] = theta_s
+            dtheta_dic[num] = dtheta_s
+            rs_dic[num] = rs
+            omega_dic[num] = omega_init
+            t_dic[num] = t + num * t_end
+            num += 1
+        theta_r_init, dtheta_r_init = theta_dic[num - 1][-1], dtheta_dic[num - 1][-1]
+
+        theta_r_dic = {}
+        dtheta_r_dic = {}
+        rs_r_dic = {}
+        omega_r_dic = {}
+        t_r_dic = {}
+        dKr = -0.2
+        Ksr = np.arange(K_end, K_start + dKr, dKr)
+
+        num_r = 0
+        for K in tqdm(Ksr):
+            if num_r == 0:
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta_r_init,
+                    p_dtheta=dtheta_r_init,
+                    p_omega=omega_init,
+                    distribution=dist,
+                )
+                # theta_s,dtheta_s,omega,rs,t = Sol_Kuramoto_mf(N,K,m,(0,t_end),dt=dt,p_theta=theta_r_init,p_dtheta=dtheta_init,p_omega=omega_init,distribution=dist)
+            else:
+                theta_s, dtheta_s, omega, rs, t = Sol_Kuramoto_mf(
+                    N,
+                    K,
+                    m,
+                    (0, t_end),
+                    dt=dt,
+                    p_theta=theta_r_dic[num_r - 1][-1],
+                    p_dtheta=dtheta_r_dic[num_r - 1][-1],
+                    p_omega=omega_r_dic[num_r - 1],
+                    distribution=dist,
+                )
+                # theta_s,dtheta_s,omega,rs,t = Sol_Kuramoto_mf(N,K,m,(0,t_end),dt=dt,p_theta=theta_r_dic[num_r-1][-1],p_dtheta=dtheta_init,p_omega=omega_r_dic[num_r-1],distribution=dist)
+            theta_r_dic[num_r] = theta_s
+            dtheta_r_dic[num_r] = dtheta_s
+            rs_r_dic[num_r] = rs
+            omega_r_dic[num_r] = omega
+            t_r_dic[num_r] = t + num_r * t_end
+            num_r += 1
+
+        createFolder(f"Review2/Noise_theta/Noise = {Noise}")
+        Time_R(
+            Ks,
+            t_dic,
+            rs_dic,
+            t_r_dic,
+            rs_r_dic,
+            dK,
+            dt,
+            t_end,
+            N,
+            m,
+            mean_time=50,
+            save=True,
+            Folder_name=f"Review2/Noise_theta/Noise = {Noise}",
+        )
