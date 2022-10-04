7dfc9b01f3db4bfb513e886af3b14260f0ee7698
Dcal-dev <58111241+Dcal-dev@users.noreply.github.com>
2022-10-04 15:20:29 +0900

    Delete Private.py
    개인정보 포함되어 있어서 local로만 유지

--- TO_sim/Private.py
+++
@@ -1,5 +0,0 @@
-from knockknock import slack_sender
-webhook_url = ("https://hooks.slack.com/services/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")
-channel="#python-notification"
-
-MY_slack_sender=slack_sender(webhook_url=webhook_url, channel=channel)
