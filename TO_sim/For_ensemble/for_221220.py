c13a80e8cd96feee2d289c15da4b74f2b8f47a21
김국영 <kimgug-young@gimgug-yeong-ui-MacBookPro.local>
2022-12-20 23:12:39 +0900

    identical oscillator에 대해서 시뮬레이션

--- TO_sim/For_ensemble/for_221220.py
+++ TO_sim/For_ensemble/for_221220.py
@@ -8,8 +8,8 @@ seed = 1
 K = 0.1
 t_end = 400
 dt = 0.1
-def get_tr(m,K,seed = 0):
-    theta_init, omega_init, Kc = Identical(N, 0, seed=seed)
+def get_tr(m,K,seed = 0,initial_speed= 0):
+    theta_init, omega_init, Kc = Identical(N, initial_speed, seed=seed)
     dtheta_init = 0*np.random.random(N)
     _,_,_, rs, t = Sol_Kuramoto_mf(N,K,m,(0, t_end),dt=dt,
                     p_theta=theta_init,
