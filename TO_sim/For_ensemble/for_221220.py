e34aae8facb49c3339686112d410de2691fbca12
김국영 <kim9young@naver.com>
2022-12-28 01:07:21 +0900

    파일 수정

--- TO_sim/For_ensemble/for_221220.py
+++ TO_sim/For_ensemble/for_221220.py
@@ -8,9 +8,8 @@ seed = 1
 K = 0.1
 t_end = 400
 dt = 0.1
-def get_tr(m,K,t_end=400,seed = 0,initial_speed= 0):
+def get_tr(m,K,t_end=400,seed = 0,initial_speed= 0, dtheta_init = np.zeros(N)):
     theta_init, omega_init, Kc = Identical(N, initial_speed, seed=seed)
-    dtheta_init = 0*np.random.random(N)
     _,_,_, rs, t = Sol_Kuramoto_mf(N,K,m,(0, t_end),dt=dt,
                     p_theta=theta_init,
                     p_dtheta=dtheta_init,
