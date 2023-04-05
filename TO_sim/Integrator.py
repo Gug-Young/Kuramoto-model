335475d2a2656a432b21a29e55ce6a8ed77ec5fc
Gugyoung Kim <kim9young@naver.com>
2023-04-05 22:59:01 +0900

    upgrade hysterisis

--- TO_sim/Integrator.py
+++ TO_sim/Integrator.py
@@ -37,22 +37,33 @@ def RK4(f, y0, t, args=()):
         y[i + 1] = y[i] + (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
     return y
 
-def RK4_r(f, y0, t, args=()):
-    n = len(t)
+def RK4_r(f, y0, t, args=(),result_time = 0):
+    n = len(t) - result_time
     y = np.zeros((n, len(y0)))
-    rs = np.zeros(n)
+    rs = np.zeros(n+result_time)
     y[0] = y0
     _,N,_,_ = args
+    h = t[1] - t[0]
     rs[0] = abs(1/N*np.sum(np.exp(1j*y0[:N])))
-
+    y_ = y0
+    j = 0
+    for i in range(result_time):
+        k1,r = f(y_, t, *args)
+        k2,_ = f(y_ + k1 * h / 2.0, t + h / 2.0, *args)
+        k3,_ = f(y_ + k2 * h / 2.0, t + h / 2.0, *args)
+        k4,_ = f(y_ + k3 * h, t + h, *args)
+        y_ = y_ + (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
+        rs[j+1] = r
+        j+=1
+    y[0] = y_
     for i in range(n - 1):
-        h = t[i + 1] - t[i]
         k1,r = f(y[i], t[i], *args)
         k2,_ = f(y[i] + k1 * h / 2.0, t[i] + h / 2.0, *args)
         k3,_ = f(y[i] + k2 * h / 2.0, t[i] + h / 2.0, *args)
         k4,_ = f(y[i] + k3 * h, t[i] + h, *args)
         y[i + 1] = y[i] + (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
-        rs[i+1] = r
+        rs[j+1] = r
+        j+=1
     return y,rs
 
 def RK4_sampling(f, y0, t,t_sample_idx, args=()):
