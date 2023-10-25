9202f169fd82958a9c6b39e37f21fc5710da436f
Gugyoung Kim <kim9young@naver.com>
2023-10-25 20:54:34 +0900

    secondary cluster re

--- TO_sim/analytical/sec_order_parameter.py
+++ TO_sim/analytical/sec_order_parameter.py
@@ -5,9 +5,6 @@ from TO_sim.gen_Distribution import Normal, Quantile_Normal as Q_Normal, Lorentz
 from scipy.integrate import quad
 from scipy.stats import norm
 
-
-
-
 def Bisection(f,r_a,r_b,eps =1e-5,end=30,arg=()):
     r_c = (r_a+r_b)/2
     num = 0 
@@ -216,13 +213,23 @@ def r_lock2(r,r_m,O_r,O_pm,K,m,g=g_sec):
         I_l,err = quad(integrand_lock,-np.pi/2,np.pi/2,epsabs=1e-10,limit=2000)
         return X*I_l    
 
+def r_drift2(r,r_m,O_r,O_pm,K,m,g=g_sec):
+    X = K*r
+    shift = lambda x,X=X: -(K**2*r*r_m)/(2*(O_pm+X*np.sin(x))**2)/m -(K**2*r*r)/(8*(O_pm+X*np.sin(x))**2)/m 
+    O_p = (4/np.pi)*np.sqrt(X/m)
+    integrand_drift = lambda x:1/(2*x**2)*g(x+shift(0),O_r,O_pm)
+    I_d,err = quad(integrand_drift,O_p,np.inf,epsabs=1e-10,limit=200)
+    I_dm,err = quad(integrand_drift,-np.inf,-O_p,epsabs=1e-10,limit=200)
+    return -X/(m)*(I_d+I_dm)
+
 def r_sec(r,r_m,O_r,O_pm,K,m,g=g_sec):
     rl = r_lock2(r,r_m,O_r,O_pm,K,m,g=g_sec)
-    rd = 0#r_drift1(r,K,m,g=g_n)
+    rd = r_drift2(r,r_m,O_r,O_pm,K,m,g=g_sec)
     return rl+rd - r
 
 def F_lock2(r,r_m,O_r,O_pm,K,m,g=g_sec):
     X = K*r
+    # shift = lambda x,X=X: -(K**2*r*r_m)/(2*(O_pm+X*np.sin(x))**2)/m -(K**2*r*r)/(8*(O_pm+X*np.sin(x))**2)/m 
     shift = lambda x,X=X: -(K**2*r*r_m)/(2*(O_pm+X*np.sin(x))**2)/m -(K**2*r*r)/(8*(O_pm+X*np.sin(x))**2)/m 
 
     integrand_lock = lambda x:np.cos(x)**2*g(X*np.sin(x)+shift(0),O_r,O_pm)
@@ -238,19 +245,51 @@ def F_lock2(r,r_m,O_r,O_pm,K,m,g=g_sec):
         I_l,err = quad(integrand_lock,-np.pi/2,np.pi/2,epsabs=1e-10,limit=2000)
         return I_l    
     
+def F_drift2(r,r_m,O_r,O_pm,K,m,g=g_sec):
+    X = K*r
+    shift = lambda x,X=X: -(K**2*r*r_m)/(2*(O_pm+X*np.sin(x))**2)/m -(K**2*r*r)/(8*(O_pm+X*np.sin(x))**2)/m 
+    O_p = (4/np.pi)*np.sqrt(X/m)
+    integrand_drift = lambda x:1/(2*x**2)*g(x+shift(0),O_r,O_pm)
+    I_d,err = quad(integrand_drift,O_p,5,limit=200)
+    return -1/(m)*I_d
+
+def F_2f(r,r_m,O_r,O_pm,K,m,g=g_sec):
+    X = K*r
+    shift = lambda x,X=X: -(K**2*r*r_m)/(2*(O_pm+X*np.sin(x))**2)/m -(K**2*r*r)/(8*(O_pm+X*np.sin(x))**2)/m 
+    O_p = (4/np.pi)*np.sqrt(X/m)
+    integrand_drift = lambda x:1/(2*x**2)*g(x+shift(0),O_r,O_pm)
+    I_d,err = quad(integrand_drift,O_p,np.inf,epsabs=1e-10,limit=200)
+    I_dm,err = quad(integrand_drift,-np.inf,-O_p,epsabs=1e-10,limit=200)
+
+    integrand_lock = lambda x:np.cos(x)**2*g(X*np.sin(x)+shift(0),O_r,O_pm)
+    omega_p = (4/np.pi)*np.sqrt(X/m)
+    A = omega_p/X
+    if abs(A)<=1:
+        theta_p = np.arcsin(A)
+        I_l,err = quad(integrand_lock,-theta_p,theta_p,epsabs=1e-10,limit=2000)
+    else: 
+        I_l,err = quad(integrand_lock,-np.pi/2,np.pi/2,epsabs=1e-10,limit=2000)
+    return -1/(2*m)*(I_d+I_dm) + I_l
+
+
 def get_F_sec(r,K,m,r_0):
     O_pm = norm.ppf(r_0/2+0.5)
     O_r = quad(norm.ppf,r_0/2+0.5,1)[0]/(0.5-r_0/2)
-    F_2 = F_lock2(r,r_0,O_r,O_pm,K,m,g_sec)
+    # F_2 = F_lock2(r,r_0,O_r,O_pm,K,m,g_sec)
+    F_2 = F_2f(r,r_0,O_r,O_pm,K,m,g_sec)
     return F_2
 F_sec = np.vectorize(get_F_sec)
 
 def get_r_sec(K,m,r_0,samples=200):
     rs_u = np.nan
     rs_d = np.nan
+
+    rs_u_l = np.nan
+    rs_u_d = np.nan
     if np.isnan(r_0):
-        return rs_u,rs_d
-    r_2test = np.linspace(0,(1-r_0)/2,samples)
+        return rs_u,rs_d,rs_u_l,rs_u_d
+    # r_2test = np.linspace(1e-4,(1-r_0)/2,samples)
+    r_2test = np.linspace(1e-4,(1-r_0)/2,samples)
     O_pm = norm.ppf(r_0/2+0.5)
     O_r = quad(norm.ppf,r_0/2+0.5,1)[0]/(0.5-r_0/2)
     Fs  = F_sec(r_2test,K,m,r_0)
@@ -258,8 +297,6 @@ def get_r_sec(K,m,r_0,samples=200):
     arg_check, = np.where(cross_point)
     near_check = np.argmin(abs(Fs[:]-1/K))
     check_err = abs(Fs[near_check] - 1/K)<1e-3
-    rs_u = np.nan
-    rs_d = np.nan
     if len(arg_check)==2:
         r_ss = []
         for arg in arg_check:
@@ -270,7 +307,10 @@ def get_r_sec(K,m,r_0,samples=200):
                 r_s = (r_a+r_b)/2
             r_ss.append(r_s)
         rs_d,rs_u = r_ss
-        return rs_d,rs_u
+        rs_u_d = r_drift2(rs_u,r_0,O_r,O_pm,K,m,g=g_sec)
+        rs_u_l = r_lock2(rs_u,r_0,O_r,O_pm,K,m,g=g_sec)
+
+        return rs_d,rs_u,rs_u_l,rs_u_d
     elif len(arg_check)==1:
         for arg in arg_check:
             r_a = r_2test[arg]
@@ -279,7 +319,9 @@ def get_r_sec(K,m,r_0,samples=200):
             if np.isnan(r_s):
                 r_s = (r_a+r_b)/2
         rs_u = r_s
-        return rs_d,rs_u
+        rs_u_d = r_drift2(rs_u,r_0,O_r,O_pm,K,m,g=g_sec)
+        rs_u_l = r_lock2(rs_u,r_0,O_r,O_pm,K,m,g=g_sec)
+        return rs_d,rs_u,rs_u_l,rs_u_d
     else:
         if check_err:
             r_a = r_2test[near_check] - 0.1
@@ -287,7 +329,9 @@ def get_r_sec(K,m,r_0,samples=200):
             r_s = Bisection(r_sec,r_a,r_b,eps=5e-3,end=15,arg = (r_0,O_r,O_pm,K,m,g_sec))
             print(r_s)
             rs_u = r_s
-        return rs_d,rs_u
+            rs_u_d = r_drift2(rs_u,r_0,O_r,O_pm,K,m,g=g_sec)
+            rs_u_l = r_lock2(rs_u,r_0,O_r,O_pm,K,m,g=g_sec)
+        return rs_d,rs_u,rs_u_l,rs_u_d
 
 
 rm_numpy = np.vectorize(get_r_mean)
@@ -300,5 +344,5 @@ warnings.filterwarnings(action='ignore')
 def make_r_rsec(m,Ks):
     r_d,r_u  = rm_numpy(Ks,m)
     r_mu = F_l1(r_u,Ks,m)*(Ks*r_u)
-    rs_d,rs_u = rs_numpy(Ks,m,r_u)
-    return m,Ks,r_d,r_u,r_mu,rs_d,rs_u
+    rs_d,rs_u,rs_u_l,rs_u_d = rs_numpy(Ks,m,r_u)
+    return m,Ks,r_d,r_u,r_mu,rs_d,rs_u,rs_u_l,rs_u_d
